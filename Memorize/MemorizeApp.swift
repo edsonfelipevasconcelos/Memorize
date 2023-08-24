9871ebb6184823768334a41f3e364dd9a6fdaf20
Edson Vasconcelos <edsonfelipe@icloud.com>
2023-08-24 13:04:05 -0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Edson Vasconcelos on 24/08/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
